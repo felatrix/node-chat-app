4b1b96eb6fd30bb9a39842b6d0f1e754ec6d2e91
Iwan Darmawan <iwansaila@gmail.com>
2018-07-30 19:42:37 +0700

    initial commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname + '/../public');
+const port = process.env.port || 3000;
+const app = express();
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(publicPath+'/index.html'));
+});
+
+app.listen(3000,()=>
+    {console.log(`running at port ${port}`);});
