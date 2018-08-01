1a8d441e8c3cb6cd64c5a2444d631329608a898d
Iwan Darmawan <iwansaila@gmail.com>
2018-08-01 12:55:28 +0700

    create generateMessage utils

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -14,13 +15,24 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New user connected');
 
+//socket.emit from admin text welcome to the chat app
+socket.emit('newMessage',generateMessage('admin','welcome to the chat app'));
+
+//socket.broadcast.emit from admin text new user joined
+socket.broadcast.emit('newMessage',{
+  from:'Admin',
+  text:'New user joined',
+  createdAt: new Date().getTime()
+});
+
   socket.on('createMessage',(message)=>{      
     console.log('create message',message);
-    io.emit('newMessage',{
-      from:message.from,
-      text:message.text,
-      createdAt:new Date().getTime()
-    });
+    io.emit('newMessage',generateMessage(message.from,message.text));
+    // socket.broadcast.emit('newMessage',{
+    //   from:message.from,
+    //   text:message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
   socket.on('disconnect',()=>{
     console.log('user disconnected');
