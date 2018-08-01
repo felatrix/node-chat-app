7da3fd387f867e41bf113b66c8f57dce3a515d43
Iwan Darmawan <iwansaila@gmail.com>
2018-08-01 17:41:50 +0700

    Add form messages and show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -14,4 +14,24 @@ socket.on('newEmail',function(){
 
 socket.on('newMessage',function(message){
     console.log('newMessage',message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}:${message.text}`);
+    jQuery('#messages').append(li);
+});
+
+// socket.emit('createMessage',{
+//     from:'Frank',
+//     text:'hei iam hank'
+// },function(data){
+//     console.log(data);
+// });
+
+jQuery('#message-form').on('submit',function(e){
+    e.preventDefault();
+    socket.emit('createMessage',{
+        from:'User',
+        text: jQuery('[name=message]').val()
+    },function(){
+
+    });
 });
