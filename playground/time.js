2bd552d5d8c4766d36cc79654b2efe46565aa4df
Iwan Darmawan <iwansaila@gmail.com>
2018-08-02 11:14:05 +0700

    add timestamp messages

---
+++ playground/time.js
@@ -0,0 +1,18 @@
+var moment = require('moment');
+
+//Jan 1st 1970 00:00:01 am
+
+// var date = new Date();
+// var months = ['Jan','Feb'];
+// console.log(date.getMonth());
+
+// var date = moment();
+// console.log(date.format('MMM YYYY'));
+
+new Date().getTime()
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp);
+
+var createdAt = 1234;
+var date = moment(createdAt);
+console.log(date);
